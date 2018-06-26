fc9402df9a19c35ae5ba5f307006ce3c14def271
Paul English <paule@mybudget.com.au>
2018-06-27 08:12:32 +0930

    finish module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,13 @@ class Quiz extends Component {
 		super(props)
 		this.state = { quiz_position: 1 };
 	}
+
+	showNextQuestion() {
+		this.setState((state) => {
+			return { quiz_position: state.quiz_position + 1 };
+		});
+	}
+
 	render() {
 		const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
 
@@ -22,7 +29,7 @@ class Quiz extends Component {
 		else {
 			return(
 				<div>
-					<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+					<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
 				</div >
 
 			);
